2b9679b805a0e596fb7ab823b55f0c51dbe38549
vagrant <you@example.com>
2019-05-16 01:18:04 +0000

    post and get request to json file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,64 +6,6 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
-const data = [
-	{
-		user: {
-			name: "Newton",
-			avatars: {
-				small:
-					"https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-				regular:
-					"https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-				large:
-					"https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-			},
-			handle: "@SirIsaac"
-		},
-		content: {
-			text:
-				"If I have seen further it is by standing on the shoulders of giants"
-		},
-		created_at: 1461116232227
-	},
-	{
-		user: {
-			name: "Descartes",
-			avatars: {
-				small:
-					"https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-				regular:
-					"https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-				large:
-					"https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-			},
-			handle: "@rd"
-		},
-		content: {
-			text: "Je pense , donc je suis"
-		},
-		created_at: 1461113959088
-	},
-	{
-		user: {
-			name: "Johann von Goethe",
-			avatars: {
-				small:
-					"https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-				regular:
-					"https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-				large:
-					"https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-			},
-			handle: "@johann49"
-		},
-		content: {
-			text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-		},
-		created_at: 1461113796368
-	}
-];
-
 function renderTweets(tweets) {
 	tweets.forEach(data => {
 		$(".tweet-container").append(createTweetElement(data));
@@ -96,12 +38,28 @@ function createTweetElement(tweet) {
 
 	return $tweet;
 }
-// createTweetElement();
+function loadTweets() {
+	$.ajax({
+		type: "GET",
+		url: "/tweets",
+		data: JSON,
+		success: function(data) {
+			renderTweets(data);
+		}
+	});
+}
 $(document).ready(function() {
-	renderTweets(data);
+	loadTweets();
+	$(function() {
+		let $newPost = $("form");
+		$newPost.submit(function(event) {
+			event.preventDefault();
+			$.ajax({
+				type: "POST",
+				url: "/tweets",
+				data: $("#submits").serialize(),
+				success: function(addPost) {}
+			});
+		});
+	});
 });
-
-// });
-// Test / driver code (temporary)
-// to see what it looks like
-// $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
